5211b2aefdd14cf7ede9f8fafc9f7d564e40818f
Naman-B-Parlecha <naman.parlecha@finalroundai.com>
2025-08-21 18:29:39 +0530

    feat: Added Logging for better log collection & Cors Configured

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/KubeOrchestra/core
 go 1.25.0
 
 require (
+	github.com/gin-contrib/cors v1.7.6
 	github.com/gin-gonic/gin v1.10.1
+	github.com/joho/godotenv v1.5.1
 	github.com/sirupsen/logrus v1.9.3
 	github.com/spf13/viper v1.20.1
 )
@@ -44,6 +46,5 @@ require (
 	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/text v0.26.0 // indirect
 	google.golang.org/protobuf v1.36.6 // indirect
-	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/KubeOrchestra/core/routes"
 	"github.com/KubeOrchestra/core/utils/config"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
 )
 
@@ -11,6 +12,7 @@ func main() {
 	if err := config.Load(); err != nil {
 		logrus.Fatalf("Failed to load configuration: %v", err)
 	}
+	logrus.SetFormatter(&logrus.JSONFormatter{})
 
 	port := config.GetPort()
 	ginMode := config.GetGinMode()
@@ -22,4 +24,8 @@ func main() {
 	if err := router.Run(":" + port); err != nil {
 		logrus.Fatal("Failed to start server:", err)
 	}
+}
+
+func init() {
+	godotenv.Load()
 }
